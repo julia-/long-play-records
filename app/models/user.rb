316e374c1e9b0fb7a65233762e759060bbbe7f90
julia- <julia.ryanx@gmail.com>
2017-11-01 14:09:08 +1100

    set up user/profile relationship

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :confirmable, :validatable
+
   has_one :profile
 end
