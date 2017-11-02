48dea96f7d4bf0135bef139ab759e27ecc196a9f
julia- <julia.ryanx@gmail.com>
2017-11-02 21:22:53 +1100

    rearrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :releases
   root 'profiles#index'
-  
+
   devise_for :users
   resources :profiles
   resources :artists
   resources :members
+  resources :releases
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
