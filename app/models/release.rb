3d1d40a3cafcc27b00aaa421bc45194263cec7ba
julia- <julia.ryanx@gmail.com>
2017-11-04 17:25:43 +1100

    search database for releases and via api

--- app/models/release.rb
+++ app/models/release.rb
@@ -1,3 +1,22 @@
 class Release < ApplicationRecord
   belongs_to :artist
+
+  def self.search_database_release(query, artist)
+    results = where("title ilike ?", "%#{query}%")
+  end
+
+  # def self.search(query, artist)
+  #   database_search = search_database_release(query, artist)
+  #
+  #   if !database_search.empty?
+  #     database_search
+  #   else
+  #     discogs_api_key = ENV.fetch('DISCOGS_API_KEY')
+  #     discogs_secret_api_key = ENV.fetch('DISCOGS_SECRET_API_KEY')
+  #
+  #     response = HTTParty.get("https://api.discogs.com/database/search?release_title=#{query}&artist=#{artist}&format=vinyl&key=#{discogs_api_key}&secret=#{discogs_secret_api_key}")
+  #
+  #     @release_data = response['results']
+  #   end
+  # end
 end
