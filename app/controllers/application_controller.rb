1674d193b880b70419497cc7b37f3f1b235cc8ea
julia- <julia.ryanx@gmail.com>
2017-11-05 12:14:17 +1100

    error message for unauthorized access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,13 @@
 class ApplicationController < ActionController::Base
   include Pundit
   protect_from_forgery with: :exception
+
+  rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+
+  private
+
+    def user_not_authorized
+      flash[:alert] = "You are not authorised to view this page"
+      redirect_to(request.referrer || root_path)
+    end
 end
