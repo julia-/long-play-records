fb2caef22661b5fc1957bba57c7fc45bf278d3ce
julia- <julia.ryanx@gmail.com>
2017-11-06 11:25:53 +1100

    set up conversation and message assocations

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,4 +1,5 @@
 class Conversation < ApplicationRecord
-  belongs_to :buyer
-  belongs_to :seller
+  belongs_to :buyer, class_name: 'User'
+  belongs_to :seller, class_name: 'User'
+  has_many :messages
 end
