3b12a27ec91ce872ead08f024d0aea6b99992175
julia- <julia.ryanx@gmail.com>
2017-11-07 23:29:21 +1100

    add default to email address for order confirmations

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,10 +1,13 @@
 class OrderMailer < ApplicationMailer
+  # default to: support_email = ENV.fetch('SUPPORT_EMAIL')
+
   def order_confirmation(buyer_email, order)
     @buyer_email = buyer_email
     @product_title = order[:title]
     @product_artist = order[:artist]
     total_amount = (order[:total_amount] / 100)
     @order_amount = total_amount
+
     mail(to: @buyer_email, subject: "Order Confirmation")
   end
 end
