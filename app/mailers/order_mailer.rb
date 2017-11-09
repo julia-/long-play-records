b0dfb23d44df73553db39f044a79fc49f0e1c597
julia- <julia.ryanx@gmail.com>
2017-11-10 01:12:28 +1100

    add rescue to mailer

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,13 +1,16 @@
 class OrderMailer < ApplicationMailer
   # default to: support_email = ENV.fetch('SUPPORT_EMAIL')
+  begin
+    def order_confirmation(buyer_email, order)
+      @buyer_email = buyer_email
+      @product_title = order[:title]
+      @product_artist = order[:artist]
+      total_amount = (order[:total_amount] / 100)
+      @order_amount = total_amount
 
-  def order_confirmation(buyer_email, order)
-    @buyer_email = buyer_email
-    @product_title = order[:title]
-    @product_artist = order[:artist]
-    total_amount = (order[:total_amount] / 100)
-    @order_amount = total_amount
-
-    mail(to: @buyer_email, subject: "Order Confirmation")
+      mail(to: @buyer_email, subject: "Order Confirmation")
+    end
+  rescue Mailgun::CommunicationError
+      nil
   end
 end
