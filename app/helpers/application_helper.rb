a3ad5ad14f1c0bbf89671da14f8e6b587bb55be4
julia- <julia.ryanx@gmail.com>
2017-11-09 11:01:56 +1100

    move converation helpers to application and create format date helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,18 @@ module ApplicationHelper
     money = Money.new(amount)
     money.format
   end
+
+  def format_time(time)
+    current_month = Date.today.month
+    current_date = Date.today.day
+    if current_date == time.day
+      time.strftime("Today, %l:%M %p")
+    else
+      time.strftime("%A %d %B, %l:%M %p")
+    end
+  end
+
+  def format_date(date)
+    date.strftime("%A %d %B, %Y")
+  end
 end
